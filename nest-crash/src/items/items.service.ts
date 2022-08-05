bfe6e554cab88b14710c2ed9fa620dfcdc959af6
Daniel Erhabor <danielnerhabor@gmail.com>
2022-08-05 19:42:50 -0400

    Hardcoded an array of items for to see if it works

--- nest-crash/src/items/items.service.ts
+++ nest-crash/src/items/items.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: '293275',
+      name: 'Item One',
+      qty: 100,
+      description: 'This is item one',
+    },
+    {
+      id: '2932234',
+      name: 'Item Two',
+      qty: 50,
+      description: 'This is item two',
+    },
+  ];
+}
