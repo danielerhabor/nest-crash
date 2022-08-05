c8d13abe73c22781b5533d24c92ceca5a3385ceb
Daniel Erhabor <danielnerhabor@gmail.com>
2022-08-05 18:39:13 -0400

    Added the `createItemDto` class to the project

--- nest-crash/src/items/items.controller.ts
+++ nest-crash/src/items/items.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export default class ItemsController {
@@ -7,7 +8,7 @@ export default class ItemsController {
     return 'Get all items';
   }
   @Post()
-  create(): string {
-    return 'Create items';
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
   }
 }
