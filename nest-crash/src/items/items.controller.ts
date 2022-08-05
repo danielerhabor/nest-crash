ad36b3e645dba75916acb99a0e7ce42c3da43610
Daniel Erhabor <danielnerhabor@gmail.com>
2022-08-05 19:28:10 -0400

    Added DELETE and PUT controller methods for the endpoints

--- nest-crash/src/items/items.controller.ts
+++ nest-crash/src/items/items.controller.ts
@@ -18,12 +18,22 @@ export default class ItemsController {
   }
 
   @Get(':id')
-  findOne(@Param(':id') id): string {
+  findOne(@Param('id') id): string {
     return `Item ${id}`;
   }
 
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
     return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id, @Body() updateItemDto: CreateItemDto): string {
+    return `Update ${id} - Name: ${updateItemDto.name}`;
   }
 }
