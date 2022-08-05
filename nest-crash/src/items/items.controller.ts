0b0558fbee1e13bb307e1c306b39b3a4a69abab5
Daniel Erhabor <danielnerhabor@gmail.com>
2022-08-05 18:50:52 -0400

    Added the `findOne` method for GET /items/:id request

--- nest-crash/src/items/items.controller.ts
+++ nest-crash/src/items/items.controller.ts
@@ -5,8 +5,7 @@ import {
   Put,
   Delete,
   Body,
-  Req,
-  Res,
+  Param,
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { Request, Response } from 'express';
@@ -14,10 +13,15 @@ import { Request, Response } from 'express';
 @Controller('items')
 export default class ItemsController {
   @Get()
-  findAll(@Req() req: Request, @Res() res: Response): Response {
-    console.log(req.url);
-    return res.send('Hello World');
+  findAll(): string {
+    return 'Get all items';
   }
+
+  @Get(':id')
+  findOne(@Param(':id') id): string {
+    return `Item ${id}`;
+  }
+
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
     return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
